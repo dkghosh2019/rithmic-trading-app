ca2140f93de50c0e078c697c8dafb16151e7f9ff
Dipak Ghosh <durja1008@gmail.com>
2026-03-18 07:16:55 -0400

    feat: bootstrap Spring Boot trading backend

---
+++ trading-backend-java/src/main/java/com/dkghosh/trading/TradingBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.dkghosh.trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingBackendJavaApplication.class, args);
+	}
+
+}
---
+++ trading-backend-java/src/test/java/com/dkghosh/trading/TradingBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkghosh.trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
